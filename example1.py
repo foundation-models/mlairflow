f0742366cacae29ef1a2ca48ae1bc46c6afab1cf
hossein20s <hossein.objectj@gmail.com>
2019-09-28 04:06:01 +0000

    Sample of mlflow

---
+++ example1.py
@@ -0,0 +1,15 @@
+import os
+from mlflow import log_artifact, log_metric, log_param
+
+if __name__ == '__main__':
+    log_param("param1", 5)
+
+    # Log a metric; metrics can be updated throughout the run
+    log_metric("foo", 1)
+    log_metric("foo", 2)
+    log_metric("foo", 3)
+
+    # Log an artifact (output file)
+    with open("output.txt", "w") as f:
+        f.write("Hello world!")
+    log_artifact("output.txt")
